fa5c86e3085de00b6453bf93e75fa576ff0314ab
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-23 10:41:53 +0300

    Variable name len removal correction

--- main.c
+++ main.c
@@ -9,14 +9,12 @@
  */
 int main(void)
 {
-int len;
-int len2;
 _printf("Character:[%c]\n", 'H');
 printf("Character:[%c]\n", 'H');
 _printf("String:[%s]\n", "I am a string !");
 printf("String:[%s]\n", "I am a string !");
-len = _printf("Percent:[%%]\n");
-len2 = printf("Percent:[%%]\n");
+_printf("Percent:[%%]\n");
+printf("Percent:[%%]\n");
 _printf("Decimal to binary:[%b]\n", 5);
 return (0);
 }
