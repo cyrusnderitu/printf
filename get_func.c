b5e0cbacff3f7f791f0aa112e511ca9ab56ab139
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-23 07:52:56 +0300

    Function addition for converting a decimal to binary

--- get_func.c
+++ get_func.c
@@ -21,6 +21,25 @@ write(1, str[i], i);
 return (i);
 }
 
+void print_bin(va_list arg)
+{
+int arr[50], x, i, j, len;
+x = va_arg(arg, int);
+while (x > 0)
+{
+for (i = 0; x > 0; i++)
+{
+arr[i] = x%2;
+x = x/2;
+len++;
+}
+for (j = i - 1; j >=0; j--)
+{
+write(1, arr[j], len);
+}
+return (len);
+}
+}
 /**
 * get_func - Picks a func in relation 
 *  to the format equal to the selector array
@@ -35,6 +54,7 @@ int (*get_func(const char format))(va_list)
 selector(array[]) = {
 {'c', print_char},
 {'s', print_str},
+{'b', print_bin},
 {'\0', NULL}
 };
 int i;
