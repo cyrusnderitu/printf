650491d70d7ee410fe03a933b6d4eaaf8ef56843
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-24 20:18:49 +0300

    Extra _putchar correction

--- main.h
+++ main.h
@@ -13,15 +13,14 @@ int _printf(const char *format, ...);
  */
 typedef struct select
 {
-	char symbol;
-	int (*f)(va_list arg);
+char symbol;
+int (*f)(va_list arg);
 } selector;
 
 int (*fetch_func(const char format))(va_list);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_bin(va_list arg);
-int _putchar(char c);
 int print_int(va_list args);
 int print_dec(va_list args);
 int print_octal(va_list arg);
