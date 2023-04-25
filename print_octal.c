058c5a35ef591bb63a6749a5de96591653eb2693
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-25 06:04:29 +0300

    Same function as binary but with a division of 8
    Converting decimal to octal takes the same functionality as that of decimal - binary but now with a division of 8

--- print_octal.c
+++ print_octal.c
@@ -7,15 +7,27 @@
  */
 int print_octal(va_list arg)
 {
-int digit, rem, oct = 0, j = 1, i;
-digit = va_arg(arg, int);
-while (digit > 0)
+long int arr[50], x, i, j, size = 0;
+x = val;
+if (x == 0)
 {
-rem = digit % 8;
-oct = oct + rem * j;
-j *= 10;
-digit /= 8;
-i++;
+putchar('0');
+return (1);
 }
-return (i);
+if (x > 0)
+{
+for (i = 0; x > 0; i++)
+{
+arr[i] = x % 8;
+x = x / 8;
+}
+for (j = i - 1; j >= 0; j--)
+{
+putchar('0' + arr[j]);
+size++;
+}
+return (size);
+}
+else
+return (-1);
 }
