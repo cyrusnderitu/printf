e3dc3fe3552b81b8055f48cb1ae2007e9609102f
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-04-25 19:35:12 -0700

    Function operation for the %p format specifier

---
+++ print_pointer.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_pointer - prints void addr given
+ * @arg: argument provided
+ * Return: Len of stdout
+ */
+int print_pointer(va_list arg)
+{
+	int x;
+	void *ptr;
+	char *nil = "(nil)";
+	ptr = va_arg(arg, void*);
+	if (p != NULL)
+	{
+		_putchar('0');
+		_putchar('x');
+		int result = print_hexa_lowercase(p);
+		return (result + 2);
+	}
+	else
+	{
+		write(1, &nil, 6);
+		return (6);
+	}
+}
