2b1732a648013f687b6293f36332fbda56c12802
LeahKamau <kamauleah35@gmail.com>
2023-04-27 03:29:31 -0700

    leave spaces after intializing

--- _printf.c
+++ _printf.c
@@ -7,40 +7,42 @@
  */
 int _printf(const char *format, ...)
 {
-unsigned int x = 0, count = 0;
-va_list lc;
-if (!format)
-return (-1);
-va_start(lc, format);
-for (x = 0; format[x] != '\0'; x++)
-{
-if (format[x] == '%')
-{
-if (format[x + 1] == '\0')
-return (-1);
-else if (format[x + 1] == '%')
-{
-_putchar('%');
-count++;
-x++;
-}
-else if (fetch_func(format[x + 1]) != NULL)
-{
-count += (fetch_func(format[x + 1]))(lc);
-x++;
-}
-else
-{
-_putchar(format[x]);
-count++;
-}
-}
-else
-{
-_putchar(format[x]);
-count++;
-}
-}
-return (count);
-va_end(lc);
+	unsigned int x = 0, count = 0;
+	va_list lc;
+
+	if (!format)
+		return (-1);
+	va_start(lc, format);
+	for (x = 0; format[x] != '\0'; x++)
+	{
+		if (format[x] == '%')
+		{
+			if (format[x + 1] == '\0')
+				return (-1);
+			else if (format[x + 1] == '%')
+			{
+				_putchar('%');
+				count++;
+				x++;
+			}
+			else if (fetch_func(format[x + 1]) != NULL)
+			{
+				count += (fetch_func(format[x + 1]))(lc);
+				x++;
+			}
+			else
+			{
+				_putchar(format[x]);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(format[x]);
+			count++;
+		}
+	}
+
+	return (count);
+	va_end(lc);
 }
