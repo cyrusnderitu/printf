d570fa5145574fad500edb15b58ce6c775307abf
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-04-28 23:00:41 +0300

    Function for the print address of %p specifier

--- print_pointer_addr.c
+++ print_pointer_addr.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
-
-int printf_hex_aux(unsigned long int num)
+/**
+ * printf_hex_addr - prints hexadecimal num.
+ * @num: arguments parameter to the function.
+ * Return: Always succes(0) else (-1).
+ */
+int print_hex_addr(unsigned long int num)
 {
-	long int i;
-	long int *array;
-	long int counter = 0;
+	long int i, *arr, size = 0;
 	unsigned long int temp = num;
 
 	while (num / 16 != 0)
 	{
 		num /= 16;
-		counter++;
+		size++;
 	}
-	counter++;
-	array = malloc(counter * sizeof(long int));
+	size++;
+	arr = malloc(size * sizeof(long int));
 
-	for (i = 0; i < counter; i++)
+	for (i = 0; i < size; i++)
 	{
-		array[i] = temp % 16;
+		arr[i] = temp % 16;
 		temp /= 16;
 	}
-	for (i = counter - 1; i >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
-		if (array[i] > 9)
-			array[i] = array[i] + 39;
-		_putchar(array[i] + '0');
+		if (arr[i] > 9)
+		{
+			arr[i] = arr[i] + 39;
+		}
+		_putchar(arr[i] + '0');
 	}
-	free(array);
-	return (counter);
+	free(arr);
+	return (size);
 }
 
 /**
@@ -38,25 +42,23 @@ int printf_hex_aux(unsigned long int num)
  */
 int print_pointer_addr(va_list val)
 {
-	void *p;
-	char *s = "(nil)";
+	void *ptr;
+	char *x = "(nil)";
 	long int a;
-	int b;
-	int i;
+	int j, i;
 
-	p = va_arg(val, void*);
-	if (p == NULL)
+	ptr = va_arg(val, void*);
+	if (ptr == NULL)
 	{
-		for (i = 0; s[i] != '\0'; i++)
+		for (i = 0; x[i] != '\0'; i++)
 		{
-			_putchar(s[i]);
+			_putchar(x[i]);
 		}
 		return (i);
 	}
-
-	a = (unsigned long int)p;
+	a = (unsigned long int)ptr;
 	_putchar('0');
 	_putchar('x');
-	b = printf_hex_aux(a);
+	j = print_hex_addr(a);
 	return (b + 2);
 }
