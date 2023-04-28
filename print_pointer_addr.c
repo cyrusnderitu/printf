3e9484e1f52098dea5657a7e61f17008f804af09
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-04-28 12:48:44 -0700

    Function printing under %p format specifier

---
+++ print_pointer_addr.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+
+int printf_hex_aux(unsigned long int num)
+{
+	long int i;
+	long int *array;
+	long int counter = 0;
+	unsigned long int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+/**
+ * printf_pointer - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_pointer_addr(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
+
+	p = va_arg(val, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = printf_hex_aux(a);
+	return (b + 2);
+}
